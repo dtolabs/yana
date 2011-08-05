df45fdaeab682189c94c5a29ba7dff2921cf91a9
ahonor <alexhonor@yahoo.com>
2011-08-05 09:05:52 -0700

    Updated docs to include an index page.

--- scripts/Docs.groovy
+++ scripts/Docs.groovy
@@ -35,7 +35,29 @@ target(figures: "Process the figures" ) {
 
 target(html: "Generates the HTML pages" ) {
    ant.mkdir ( dir: "${DIST}/html" )
-   depends( figures )
+   depends( figures, userguide, apimanual, refpages )
+   ant.echo ( message: 'Generating the index ...') 
+   ant.copy ( file: "docs/en/index.md.template",
+              tofile: "${DIST}/html/index.md", filtering:true )
+  
+   // Concat the index files and generate index.html
+   ant.exec ( executable: PANDOC ) {
+     arg ( value: "-s" )
+     arg ( value: "${DIST}/html/index.md" )
+     arg ( value: "${DIST}/html/man5.index.md" )
+     arg ( value: "--css=docs.css" )
+     arg ( value: "--template=docs/templates/html.template" )
+     arg ( value: "--include-before=docs/templates/before.html" )
+     arg ( value: "--include-after=docs/templates/after.html" )
+     arg ( value: "-o" )
+     arg ( value: "${DIST}/html/index.html" )
+   }
+   ant.echo ( message: "Completed: ${DIST}/html/index.html")
+
+   
+}
+
+target(userguide: "Generates the HTML pages" ) {
    ant.echo ( message: 'Generating the User-Guide ...' )
    ant.exec ( executable: PANDOC ) {
      arg ( value: "--number-sections" )
@@ -53,7 +75,9 @@ target(html: "Generates the HTML pages" ) {
      arg ( value: "${DIST}/html/User-Guide.html" )
    }
    ant.echo ( message: "Completed: ${DIST}/html/User-Guide.html")
+}
 
+target(apimanual: "Generate the API-Manual") {
    ant.echo ( message: 'Generating the API-Manual ...' )
    ant.exec ( executable: PANDOC ) {
      arg ( value: "--number-sections" )
@@ -69,7 +93,9 @@ target(html: "Generates the HTML pages" ) {
      arg ( value: "${DIST}/html/API-Manual.html" )
    }
    ant.echo ( message: "Completed: ${DIST}/html/API-Manual.html" )
+}
 
+target(refpages: "Generate the reference pages") {
    ant.echo ( message: 'Generating reference pages ...' )
    ant.exec ( executable: PANDOC ) {
      arg ( value: "--number-sections" )
@@ -85,6 +111,14 @@ target(html: "Generates the HTML pages" ) {
      arg ( value: "${DIST}/html/node-v10.html" )
    }
    ant.echo ( message: "Completed: ${DIST}/html/node-v10.html")
+   
+   ant.echo ( append:false,file: "${DIST}/html/man5.index.md", 
+              message: "\n## File Formats\n" )
+   new File( "docs/en/manpages/man5" ).eachFile{file->
+     def basename = file.name.split('\\.md')[0]
+     ant.echo ( append:true, file:"${DIST}/html/man5.index.md",
+                message: "* [${basename}](${basename}.html)")
+   }
 }
 
 setDefaultTarget(main)
